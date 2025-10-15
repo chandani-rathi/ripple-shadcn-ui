f3c25ee268331613e04b8372eccd1afd7369bad1
Chadni Rathi <chandani.rathi@gmail.com>
2025-10-16 00:25:40 +0530

    added separator

--- primitives/ripple/ripple-primitive/src/index.ts
+++ primitives/ripple/ripple-primitive/src/index.ts
@@ -1,4 +1,5 @@
 export * from '@ripple-primitives/label';
 export * from '@ripple-primitives/toggle';
 export * from '@ripple-primitives/accessible-icon';
-export * from "@ripple-primitives/visually-hidden"
+export * from "@ripple-primitives/visually-hidden";
+export * from "@ripple-primitives/separator";

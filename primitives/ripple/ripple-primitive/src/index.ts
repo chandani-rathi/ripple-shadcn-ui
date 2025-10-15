de45bc4aab43d573485d65bd70df07d6bf0f64f1
Chadni Rathi <chandani.rathi@gmail.com>
2025-10-15 20:51:13 +0530

    added visually hidden primitive

--- primitives/ripple/ripple-primitive/src/index.ts
+++ primitives/ripple/ripple-primitive/src/index.ts
@@ -1,3 +1,4 @@
 export * from '@ripple-primitives/label';
 export * from '@ripple-primitives/toggle';
 export * from '@ripple-primitives/accessible-icon';
+export * from "@ripple-primitives/visually-hidden"
